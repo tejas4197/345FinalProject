d5a72cdc56d3dd5a2b285eb58fee8d2c033ee0a5
Drew Graham <Drew Graham>
2018-08-09 15:08:27 -0400

    added Command Pattern assets to project

---
+++ Unity Project/Assets/Scripts/Actor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+An Actor is a controllable entity in the scene, either the player or someone else. 
+
+The only difference between the player character and an identically-configured actor is whether 'isPlayer' is set 
+to true or false. 
+
+Ideally this class should also be used for enemies, even if they shouldn't be controlled (add a 'controllable' 
+bool to class to handle this)
+
+*/
+
+public class Actor : MonoBehaviour {
+	/// <summary>
+	/// Actor health
+	/// </summary>
+	public float health;
+	
+	/// <summary>
+	/// True if actor is player character
+	/// </summary>
+	public bool isPlayer;
+
+    /// <summary>
+    /// Contains objects that are used by the actor
+    /// </summary>
+    public List<GameObject> inventory;
+
+    /// <summary>
+    /// List of commands available to the actor
+    /// </summary>
+    public List<Command.Type> actionQueue;
+
+    /// <summary>
+    /// Text label displayed below actor (TODO: remove this from future projects)
+    /// Collection of commands to execute
+    /// </summary>
+    public List<Command> commands;
+
+    private void Start()
+    {
+        // Initialize text label below actor
+    }
+}
---
+++ Unity Project/Assets/Scripts/MoveComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+Attach this to an Actor to allow movement via execution of MoveCommand
+
+ */
+
+public class MoveComponent : MonoBehaviour {
+
+	/// <summary>
+	/// Movement speed
+	/// </summary>
+	public float moveSpeed = 5;
+
+	protected Animator animator;
+
+	/// <summary>
+	/// Direction player is currently facing
+	/// </summary>
+	public BaseConstants.Direction currentDirection { get; protected set;}
+
+	protected void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+    /// <summary>
+    /// Moves/animates actor based on provided horizontal/vertical input from MoveCommand
+    /// </summary>
+    /// <param name="input">Player input</param>
+    public virtual void ManageMovement(Vector2 input)
+    {
+        Debug.LogError(name + " | MoveComponent.MangeMovement() not implemented");
+    }
+
+    /// <summary>
+    /// Triggers jump (if implemented in derived class)
+    /// </summary>
+    public virtual void Jump()
+    {
+        Debug.LogError(name + " | MoveComponent.Jump() not implemented");
+    }
+}
