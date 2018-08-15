0d4bbae17d11f40ae08053939ca04eb211eaffc2
Drew Graham <Drew Graham>
2018-08-14 20:13:04 -0400

    enemy merge spherecollider is created on enemy spawn

---
+++ Unity Project/Assets/Scripts/Commands/EnemyMerge.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMerge : MonoBehaviour 
+{
+	/// <summary>
+	/// Merge bounds
+	/// </summary>
+	SphereCollider mergeBounds;
+
+	/// <summary>
+	/// Radius required to merge
+	/// </summary>
+	public float mergeRadius;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Create child object positioned on enemy 
+		GameObject child = new GameObject("Merge Bounds");
+		child.transform.SetParent(transform);
+		child.transform.localPosition = Vector3.zero;
+
+		// Add merge bounds collider
+		mergeBounds = child.AddComponent<SphereCollider>();
+		if(!mergeBounds) {
+			GameController.LogWarning("Merge collider not found for " + name + ": please add a SphereCollider to a child of this GameObject");
+			return;
+		}
+		
+		// Set radius
+		mergeBounds.radius = mergeRadius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
