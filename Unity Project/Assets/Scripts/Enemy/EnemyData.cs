21ab603a7c0120dae3c0178ee62031776b0790ff
Drew Graham <Drew Graham>
2018-08-21 16:34:24 -0400

    added separate dictionaries for merge compatibility and merge results

--- Unity Project/Assets/Scripts/Enemy/EnemyData.cs
+++ Unity Project/Assets/Scripts/Enemy/EnemyData.cs
@@ -7,14 +7,22 @@ public class EnemyData : ScriptableObject {
 	/// <summary>
 	/// Dictionary of compatible colors for merging
 	/// </summary>
-	public Dictionary<Actor.Color, List<Actor.Color>> mergeDictionary = new Dictionary<Actor.Color, List<Actor.Color>> {
-		{Actor.Color.RED, new List<Actor.Color>{Actor.Color.GREEN, Actor.Color.BLUE}},
-		{Actor.Color.GREEN, new List<Actor.Color>{Actor.Color.RED, Actor.Color.BLUE}},
-		{Actor.Color.BLUE, new List<Actor.Color>{Actor.Color.RED, Actor.Color.GREEN}},
+	public Dictionary<Actor.Color, List<Actor.Color>> mergeCompatible = new Dictionary<Actor.Color, List<Actor.Color>> {
+		{ Actor.Color.RED, new List<Actor.Color>{Actor.Color.GREEN, Actor.Color.BLUE} },
+		{ Actor.Color.GREEN, new List<Actor.Color>{Actor.Color.RED, Actor.Color.BLUE} },
+		{ Actor.Color.BLUE, new List<Actor.Color>{Actor.Color.RED, Actor.Color.GREEN} },
 
-		{Actor.Color.CYAN, new List<Actor.Color>{Actor.Color.MAGENTA, Actor.Color.YELLOW}},
-		{Actor.Color.MAGENTA, new List<Actor.Color>{Actor.Color.CYAN, Actor.Color.YELLOW}},
-		{Actor.Color.YELLOW, new List<Actor.Color>{Actor.Color.CYAN, Actor.Color.MAGENTA}},
+		{ Actor.Color.CYAN, new List<Actor.Color>{Actor.Color.MAGENTA, Actor.Color.YELLOW} },
+		{ Actor.Color.MAGENTA, new List<Actor.Color>{Actor.Color.CYAN, Actor.Color.YELLOW} },
+		{ Actor.Color.YELLOW, new List<Actor.Color>{Actor.Color.CYAN, Actor.Color.MAGENTA} },
+	};
+
+	public Dictionary<List<Actor.Color>, Actor.Color> mergeResult = new Dictionary<List<Actor.Color>, Actor.Color> {
+		{ new List<Actor.Color>{Actor.Color.RED, Actor.Color.GREEN}, Actor.Color.YELLOW },
+		{ new List<Actor.Color>{Actor.Color.RED, Actor.Color.BLUE}, Actor.Color.MAGENTA },
+		{ new List<Actor.Color>{Actor.Color.GREEN, Actor.Color.BLUE}, Actor.Color.CYAN },
+
+		{ new List<Actor.Color>{Actor.Color.CYAN, Actor.Color.MAGENTA, Actor.Color.YELLOW}, Actor.Color.WHITE },
 	};
 
 	/// <summary>
@@ -25,9 +33,9 @@ public class EnemyData : ScriptableObject {
 	/// <returns></returns>
 	public bool CanMerge(Actor.Color one, Actor.Color another)
 	{
-		if(mergeDictionary.ContainsKey(one) && mergeDictionary.ContainsKey(another))
+		if(mergeCompatible.ContainsKey(one) && mergeCompatible.ContainsKey(another))
 		{
-			return mergeDictionary[one].Contains(another);
+			return mergeCompatible[one].Contains(another);
 		}
 		// Returns false for black/white (not in dicitonary)
 		return false;
