b02e014df16ab110e9f96d60df46a3e6abbe6fe9
Drew Graham <dmg9626@gmail.com>
2018-08-26 01:28:55 -0400

    updated NavMeshSurface, added EnemySpawner script

---
+++ Unity Project/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [System.Serializable]
+    public class SpawnSettings {
+        public float minSpawnTime;
+        public float maxSpawnTime;
+
+        public List<Actor.Color> enemyTypes;
+    }
+    public SpawnSettings settings;
+
+    /// <summary>
+    /// Radius within which enemies can spawn
+    /// </summary>
+    public float spawnRadius;
+
+    /// <summary>
+    /// List of enemy types that can spawn
+    /// </summary>
+
+    public EnemyController enemyController;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
