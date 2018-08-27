f5e75e0c111bbecfba946f9d1386e639e86cf89f
Wills-M <wmm38@drexel.edu>
2018-08-27 16:25:47 -0400

    Cyan enemy has a ranged attack that shoots around it. Also lowered it's speed because it was a bit obnoxious at its current speed (and was also outrunning its bullets).

---
+++ Unity Project/Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour {
+
+    public float bulletDamage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        Actor actor = collision.gameObject.GetComponent<Actor>();
+        if (actor && actor.isPlayer)
+        {
+            actor.TakeDamage(gameObject, bulletDamage);
+        }
+    }
+}
---
+++ Unity Project/Assets/Scripts/CyanAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyanAttack : MonoBehaviour {
+
+    public GameObject bullet;
+    public float fireCooldown;
+    public float bulletSpeed;
+
+    private Vector3[] vectors;
+    private float timer = 0;
+
+    public void Start()
+    {
+        // Randomly decide if this will attack at an angle or not
+        if (Random.value < 0.5f)
+            vectors = new Vector3[] { Vector3.forward, Vector3.back, Vector3.right, Vector3.left };
+        else
+            vectors = new Vector3[] { Quaternion.AngleAxis(45, Vector3.up) * Vector3.right,
+                                      Quaternion.AngleAxis(135, Vector3.up) * Vector3.right,
+                                      Quaternion.AngleAxis(225, Vector3.up) * Vector3.right,
+                                      Quaternion.AngleAxis(315, Vector3.up) * Vector3.right};
+    }
+
+    public void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= fireCooldown)
+        {
+            foreach(Vector3 v in vectors)
+            {
+                GameObject currBullet = Instantiate(bullet, gameObject.transform.position + v, Quaternion.identity);
+                currBullet.GetComponent<Rigidbody>().AddForce(v * bulletSpeed);
+            }
+            
+            timer = 0;
+        }
+    }
+}
