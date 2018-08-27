6257607f531fc3bb9d5cbb23db902384a54b75e3
Wills-M <wmm38@drexel.edu>
2018-08-27 17:42:23 -0400

    Magenta enemies now shoot a single bullet towards player. Requires a new player singleton to be on the player so it can locate it.

---
+++ Unity Project/Assets/Scripts/MagentaAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MagentaAttack : MonoBehaviour {
+
+    public GameObject bullet;
+    public float range;
+    public float bulletSpeed;
+    public float fireCooldown;
+
+    private RaycastHit hit;
+    private float timer = 0;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > fireCooldown && Physics.Linecast(gameObject.transform.position, PlayerRef.Instance.transform.position, out hit) && hit.distance <= range)
+        {
+            Vector3 towards = PlayerRef.Instance.transform.position - gameObject.transform.position;
+            towards.Normalize();
+            GameObject currBullet = Instantiate(bullet, gameObject.transform.position + towards, Quaternion.identity);
+            currBullet.GetComponent<Rigidbody>().AddForce(towards * bulletSpeed);
+
+            timer = 0;
+        }
+    }
+}
---
+++ Unity Project/Assets/Scripts/PlayerRef.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRef : MonoBehaviour {
+
+    #region Singleton
+    public static PlayerRef Instance { get; private set; }
+
+    //If an instance of this singleton exists, then destroy the gameobject (this means there are more than one)
+    //If an instance doesn't exist, create one
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.Log("Warning: found more than one instance of PlayerRef Singleton. Destroying " + gameObject.name + " gameobject.");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+}
