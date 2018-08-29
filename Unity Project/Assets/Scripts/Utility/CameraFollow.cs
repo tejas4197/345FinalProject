030e3aed53d7fb1a19e0a086bc733ad17d089a84
Drew Graham <Drew Graham>
2018-08-29 15:59:28 -0400

    added smooth camera follow

--- Unity Project/Assets/Scripts/Utility/CameraFollow.cs
+++ Unity Project/Assets/Scripts/Utility/CameraFollow.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
-    public GameObject player;
+    /// <summary>
+    /// Reference to player
+    /// </summary>
+    public Transform player;
+
+    /// <summary>
+    /// Offset between player and camera (saved at Start())
+    /// </summary>
     private Vector3 offset_;
+
+    /// <summary>
+    /// Used to control speed of linear interpolation
+    /// </summary>
+    public float smoothSpeed;
 
 
     void Start()
     {
         // Save offset at game start
-        offset_ = transform.position - player.transform.position; //+ offset;
+        offset_ = transform.position - player.position; //+ offset;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        // Adjust camera towards player based on offset
-        transform.position = player.transform.position + offset_;
+        // Get desired position towards player based on offset
+        Vector3 newPos = player.position + offset_;
+        
+        // Smooth current position towards newPos using linear interpolation
+        transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed);
     }
 }
 
