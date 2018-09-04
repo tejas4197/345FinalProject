3502dcb749a4ddd8c2a41fa75d9047d663e6096d
Drew Graham <Drew Graham>
2018-09-04 19:21:48 -0400

    hooked up UI to player color controller - now displaying current color

--- Unity Project/Assets/Scripts/PlayerColorController.cs
+++ Unity Project/Assets/Scripts/PlayerColorController.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerColorController : MonoBehaviour {
 
     #region Singleton
     public static PlayerColorController Instance { get; private set; }
+
+    /// <summary>
+    /// Red color indicator
+    /// </summary>
+    public Text redText;
+
+    /// <summary>
+    /// Green color indicator
+    /// </summary>
+    public Text greenText;
+    
+    /// <summary>
+    /// Blue color indicator
+    /// </summary>
+    public Text blueText;
 
     //If an instance of this singleton exists, then destroy the gameobject (this means there are more than one)
     //If an instance doesn't exist, create one
@@ -32,10 +48,21 @@ public class PlayerColorController : MonoBehaviour {
         set
         {
             _color = value;
-            if (DoorManager.Instance)
-            {
+
+            // Check if we have enough color to unlock door
+            if (DoorManager.Instance) {
                 DoorManager.Instance.CheckDoors();
             }
+
+            // Update UI
+            UpdateUI(_color);
         }
+    }
+
+    void UpdateUI(ColorModel color)
+    {
+        redText.text = "Red: " + color.Red;
+        greenText.text = "Green: " + color.Green;
+        blueText.text = "Blue: " + color.Blue;
     }
 }
