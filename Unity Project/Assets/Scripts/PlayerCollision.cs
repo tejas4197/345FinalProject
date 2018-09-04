d85cc10d9ed7f8b8fc28ff9da77a0155de5a164f
Drew Graham <Drew Graham>
2018-09-04 19:45:20 -0400

    on collision, player receives knockback and ignores subsequent damage for a short period of time

--- Unity Project/Assets/Scripts/PlayerCollision.cs
+++ Unity Project/Assets/Scripts/PlayerCollision.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour {
 
     private Actor actor;
+    private Rigidbody rbody;
+
+    public float reboundForce;
+
+    public float ignoreDamageTime;
+
+    /// <summary>
+    /// Ignore damage when true
+    /// </summary>
+    bool ignoreDamage;
 
     private void Start()
     {
         actor = GetComponent<Actor>();
+        rbody = GetComponent<Rigidbody>();
+
+        ignoreDamage = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag.Equals("Enemy"))
+        if (!ignoreDamage && collision.gameObject.tag.Equals("Enemy"))
         {
             actor.TakeDamage(collision.gameObject, collision.gameObject.GetComponent<Actor>().meleeDamage);
+
+            Vector3 fromEnemy = transform.position - collision.transform.position;            GameController.LogWarning("Vector magnitude: " + fromEnemy.magnitude);
+            // GameController.LogWarning("Receiving force from direction " + string.Format("{0}, {1}, {2}", fromEnemy.x, fromEnemy.y, fromEnemy.z));
+            rbody.AddForce(fromEnemy * reboundForce, ForceMode.Impulse);
+
+            // Ignore damage for a moment
+            StartCoroutine(IgnoreDamage(ignoreDamageTime));
         }
+    }
+
+    /// <summary>
+    /// Player ignores any damage for a given number of seconds
+    /// </summary>
+    /// <param name="time">Duration</param>
+    private IEnumerator IgnoreDamage(float time)
+    {
+        ignoreDamage = true;
+        GameController.Log("Ignoring damage for " + time + " seconds");
+        yield return new WaitForSeconds(time);
+        GameController.Log("No longer ignoring damage");
+        ignoreDamage = false;
     }
 }
